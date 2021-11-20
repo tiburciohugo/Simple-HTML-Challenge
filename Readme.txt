e8c8dac5f7e8c593dfa7c82cd71046af02908c46
tiburciohugo <hugotiburcio@outlook.com>
2021-11-20 16:38:29 -0300

    add

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+Create a simple page that contains:
+- 2 headings;
+- 1 paragraph;
+- Navigation with listed items;
+- A Section;
+- Footer;
---
+++ script.js
@@ -0,0 +1,8 @@
+// const document = document
+// let logo = document.getElementsByClassName("logo");
+
+// // function changeElement(element) {
+// //   element.innerHTML = 'Logo'
+// // }
+
+// logo.innerHTML = "Logo"
